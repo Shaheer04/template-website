f81a339d409d5228b45522399351057525d4e96c
Shaheer Jamal <112032384+Shaheer04@users.noreply.github.com>
2024-01-27 15:53:52 +0000

    changes v2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "<div styleName="" />
-<images />
-<technology className=""></technology>jpg";
+import image from "../images/technology.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
